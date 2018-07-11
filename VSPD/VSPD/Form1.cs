bb555e74192cd97502202600a9cbe340a4a12d72
pm-WeiYu <40255016+pm-WeiYu@users.noreply.github.com>
2018-07-11 15:09:02 +0800

    C#串口通信基础
    基础版本

---
+++ VSPD/VSPD/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSPD
+{
+
+
+    public partial class Form1 : Form
+    {
+        //创建串口接收输入对象
+        private PortControlHelper pchSend;
+        private PortControlHelper pchReceive;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pchSend = new PortControlHelper();
+            pchReceive = new PortControlHelper();
+            VSPDopen();
+        }
+
+
+        /// <summary>
+        /// 打开串口
+        /// </summary>
+        /// 
+        void VSPDopen()
+        {
+
+            //pchSend.ClosePort();
+            //pchReceive.ClosePort();
+
+            pchSend.openPort("COM2");
+            pchReceive.openPort("COM3");
+
+            pchReceive.OnComReceiveDataHandler += new PortControlHelper.ComReceiveDataHandler(ComReceiveData);
+        }
+
+
+        /// <summary>
+        /// 接收到的数据，写入文本框内
+        /// </summary>
+        /// <param name="data"></param>
+        private void ComReceiveData(string data)
+        {
+            this.Invoke(new EventHandler(delegate
+            {
+                textBox2.AppendText($"接收：{data}\n");
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pchSend.SendData(textBox1.Text);
+        }
+
+        /// <summary>
+        /// 关闭端口
+        /// </summary>
+
+
+
+
+    }
+}
---
+++ VSPD/VSPD/PortControlHelper.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSPD
+{
+
+    class PortControlHelper
+    {
+        //创建串口对象
+        private SerialPort sp;
+
+        /// <summary>
+        /// 端口名称数组
+        /// </summary>
+        public string[] PortNameArr { get; set; }
+
+        /// <summary>
+        /// 串口通信开启状态
+        /// </summary>
+        //public bool PortState { get; set; } = false;
+
+        /// <summary>
+        /// 编码类型
+        /// </summary>
+        public Encoding EncodingType { get; set; } = Encoding.ASCII;
+
+        /// <summary>
+        /// 串口接收数据委托
+        /// </summary>
+        public delegate void ComReceiveDataHandler(string data);
+
+        public ComReceiveDataHandler OnComReceiveDataHandler = null;
+
+        public PortControlHelper()
+        {
+            PortNameArr = SerialPort.GetPortNames();
+            sp = new SerialPort();
+            //sp.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        /// <param name="sendData"></param>
+        public void SendData(string sendData)
+        {
+            try
+            {
+                sp.Encoding = EncodingType;
+                sp.Write(sendData);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 打开窗口
+        /// </summary>
+        public void openPort(string name) {
+            //string portName, int boudRate = 115200, int dataBit = 8, int stopBit = 1, int timeout = 5000
+            sp.PortName = name;
+            sp.BaudRate = 110;
+            sp.DataBits = 8;
+            sp.StopBits = (StopBits)1;
+            sp.ReadTimeout = 5000;
+            sp.Open();
+
+        }
+
+        /// <summary>
+        /// 关闭端口
+        /// </summary>
+        public void ClosePort()
+        {
+            try
+            {
+                sp.Close();
+                //PortState = false;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 接收数据回调用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] buffer = new byte[sp.BytesToRead];
+            sp.Read(buffer, 0, buffer.Length);
+            string str = EncodingType.GetString(buffer);
+            if (OnComReceiveDataHandler != null)
+            {
+                OnComReceiveDataHandler(str);
+            }
+        }
+
+    }
+}
